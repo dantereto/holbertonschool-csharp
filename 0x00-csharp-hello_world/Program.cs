cb989babe32bdd908cbdae546451f658f9c3c315
dantereto <2247@holbertonschool.com>
2021-11-29 16:33:21 -0500

    hello

---
+++ 0x00-csharp-hello_world/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _0x00_csharp_hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
