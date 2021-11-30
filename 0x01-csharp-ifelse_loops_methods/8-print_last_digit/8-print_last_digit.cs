f67391515df27c4b735f9c9096d5027c8f747082
dantereto <2247@holbertonschool.com>
2021-11-30 17:06:59 -0500

    last-digit

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+    class Number
+    {
+        public static int PrintLastDigit(int number)
+        {
+            int n = number % 10;
+            if (n < 0)
+                n = n * -1;
+            Console.Write(n);
+            return n;
+        }
+    }
