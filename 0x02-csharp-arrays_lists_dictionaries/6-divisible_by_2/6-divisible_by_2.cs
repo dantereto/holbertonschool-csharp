d07b70759f0d63e328346187e308803133f06f9a
dantereto <2247@holbertonschool.com>
2021-12-01 18:37:20 -0500

    check divisible

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> iList = new List<bool>();
+        foreach (int i in myList)
+        {
+            if (i % 2 == 0)
+                iList.Add(true);
+            else
+                iList.Add(false);
+        }
+        return iList;
+    }
+
+}
