2a878e546af4839229d3ce61833a0d0784729b8f
dantereto <2247@holbertonschool.com>
2021-12-01 16:16:39 -0500

    find element

--- 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -5,7 +5,7 @@ class Array
     public static int elementAt(int[] array, int index)
     {
         if (index < 0 || index >= array.Length){
-            Console.Write("Index out of range");
+            Console.WriteLine("Index out of range");
             return -1;
         }
         return array[index];
