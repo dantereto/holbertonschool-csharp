8a099ec35919579aeda6f60379e02e16a2eb1dd7
dantereto <2247@holbertonschool.com>
2021-12-01 17:10:40 -0500

    reverse array

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array == null){
+            Console.WriteLine();
+            return;
+        }
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            if (i == 0)
+                Console.Write(array[i]);
+            else
+                Console.Write("{0} ", array[i]);
+        }
+        Console.WriteLine();
+    }
+}
+
