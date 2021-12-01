4509c60557abe9e58c28cca98818bc14e2a3096a
dantereto <2247@holbertonschool.com>
2021-12-01 18:09:22 -0500

    print list

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+            {
+                Console.WriteLine("Size cannot be negative");
+                return null;
+            }
+        List<int> iList = new List<int>();
+        for(int i = 0; i < size; i++)
+        {
+            iList.Add(i);
+            if (i != size -1)
+                Console.Write("{0} ", i);
+            else
+                Console.Write(i);
+        }
+        Console.WriteLine();
+        return iList;
+    }
+}
