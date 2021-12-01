843bfb274e4f6d08aab92e6ce8f7ee5d17975d80
dantereto <2247@holbertonschool.com>
2021-12-01 16:14:01 -0500

    find element

---
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Array
+{
+    public static int elementAt(int[] array, int index)
+    {
+        if (index < 0 || index >= array.Length){
+            Console.Write("Index out of range");
+            return -1;
+        }
+        return array[index];
+    }
+}
+
