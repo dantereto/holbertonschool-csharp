3ef21ec09103df3492c582bfb631ad56c7bb4fb4
dantereto <2247@holbertonschool.com>
2021-12-02 16:57:31 -0500

    Queue

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: {0}", aQueue.Count);
+        if (aQueue.Count <= 0)
+            Console.WriteLine("Stack is empty");
+        else
+        {
+            string top_queue = aQueue.Peek();
+            Console.WriteLine("Top item: {0}", top_queue);
+        }
+        if (aQueue.Contains(search) == true)
+        {
+            Console.WriteLine("Queue contains \"{0}\": True", search);
+        }
+        else if (aQueue.Contains(search) == false)
+            Console.WriteLine("Queue contains \"{0}\": False", search);
+        if (aQueue.Contains(search))
+        {
+            while(aQueue.Contains(search))
+            {
+                aQueue.Dequeue();
+            }
+        }
+        aQueue.Enqueue(newItem);
+        return aQueue;
+    }
+}
