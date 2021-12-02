46254c630a77d3eeeaa33e5b2d64035df7f80b32
dantereto <2247@holbertonschool.com>
2021-12-02 18:53:22 -0500

    pop

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        if (myLList.Count == 0)
+            return 0;
+        LinkedListNode<int> node = myLList.First;
+        myLList.Remove(myLList.First);
+        return node.Value;
+    }
+}
