0083482846e71232e7d086c1d818dc24adf2132c
dantereto <2247@holbertonschool.com>
2021-12-01 21:22:03 -0500

    jeys of a dict

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int cont = 0;
+        foreach(string key in myDict.Keys)
+            cont++;
+        return cont;
+    }
+}
