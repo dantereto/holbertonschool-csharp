bed6d6b18dc4dfbbaca6f2bab72b4e71f7a09785
dantereto <2247@holbertonschool.com>
2021-12-02 18:14:30 -0500

    linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static LinkedListNode<int> Add(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> ndode = myLList.AddFirst(n);
+        return ndode;
+    }
+}
