5d26df61f5cfe35034fba5ce1fddaf3ee4d33272
dantereto <2247@holbertonschool.com>
2021-12-02 15:32:03 -0500

    different elements

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        List<int> copy = new List<int>();
+        for (int i = 0; i < list1.Count; i++){
+            if (list2.Contains(list1[i]) == false){
+                copy.Add(list1[i]);
+            }
+        }
+        for (int i = 0; i < list2.Count; i++){
+            if (list1.Contains(list2[i]) == false){
+                copy.Add(list2[i]);
+            }
+        }
+        copy.Sort();
+        return copy;
+    }
+}
