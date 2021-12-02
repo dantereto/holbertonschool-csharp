973686a824a0e45056fe434894b29f70025bb9bc
dantereto <2247@holbertonschool.com>
2021-12-02 16:42:57 -0500

    push stack

--- 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -5,10 +5,13 @@ class MyStack
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
         Console.WriteLine("Number of items: {0}", aStack.Count);
-        string top_stack = aStack.Peek();
-        Console.WriteLine("Top item: {0}", top_stack);
         if (aStack.Count <= 0)
             Console.WriteLine("Stack is empty");
+        else
+        {
+            string top_stack = aStack.Peek();
+            Console.WriteLine("Top item: {0}", top_stack);
+        }
         if (aStack.Contains(search) == true)
         {
             Console.WriteLine("Stack contains \"{0}\": True", search);
