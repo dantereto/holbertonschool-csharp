a5dc38837f9996accb519946af15a9677c257a09
dantereto <2247@holbertonschool.com>
2021-12-02 14:33:17 -0500

    only unique

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        int sum = 0;
+        List<int> copy = new HashSet<int>(myList).ToList();
+        for (int i = 0; i < myList.Count; i++)
+        {
+            sum += myList[i];
+        }
+        return sum;
+    }
+}
