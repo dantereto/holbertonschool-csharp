5fedc80e93ba965d8ab51041a8ad3105a4eeb182
dantereto <2247@holbertonschool.com>
2021-12-01 20:19:33 -0500

    delete

---
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+{
+    public static List<int> DeleteAt(List<int> myList, int index)
+    {
+        if (index < 0 || index >= myList.Count){
+            Console.WriteLine("Index out of range");
+            return myList;
+        }
+        for (int i = 0; i <= myList.Count; i++){
+            if (i == index){
+                myList.Remove(myList[index]);
+            }
+        }
+        return myList;
+    }
+}
