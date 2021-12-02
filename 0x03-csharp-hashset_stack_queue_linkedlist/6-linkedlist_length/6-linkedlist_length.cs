896753f53f5611444e6110978f3258e003eeccbb
dantereto <2247@holbertonschool.com>
2021-12-02 17:37:16 -0500

    ll_list lenght

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        int cont = 0;
+        foreach(int data in myLList){
+            cont += 1;
+        }
+        return cont;
+    }
+}
