f0c27173d864717dbec5d5142a302fda2d5702a7
dantereto <2247@holbertonschool.com>
2021-12-01 23:00:31 -0500

    best score

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        int max = -1;
+        string key = "None";
+        foreach (KeyValuePair<string, int> data in myList)
+        {
+            if (data.Value > max)
+            {
+                max = data.Value;
+                key = data.Key;
+            }
+        }
+        return key;
+    }
+    
+}
