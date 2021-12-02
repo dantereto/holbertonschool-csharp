f99d526b9ccc2b815c7799c1bcfc88fa4e3c48f3
dantereto <2247@holbertonschool.com>
2021-12-01 22:45:07 -0500

    sorted

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        List<string> k_list = new List<string>();
+        foreach (string data in myDict.Keys)
+            k_list.Add(data);
+        k_list.Sort();
+        foreach (string data in k_list){
+            Console.WriteLine("{0}: {1}", data, myDict[data]);
+        }
+
+    }
+    
+}
