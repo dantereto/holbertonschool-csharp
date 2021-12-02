52e2d2f45b8d7363a5fa5744f7fdce1aa9e6c59c
dantereto <2247@holbertonschool.com>
2021-12-01 22:22:52 -0500

    multiply 2

---
+++ 0x02-csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+class Dictionary
+{
+    public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
+    {
+        Dictionary<string, int> copy = new Dictionary<string, int>(myDict);
+        foreach (var data in myDict){
+            copy[data.Key] = data.Value * 2;
+        }
+        return copy;
+    }
+}
