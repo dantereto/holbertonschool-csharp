d9ae40fe09f0a12f1df8193846fbcb5a65ae9a1c
dantereto <2247@holbertonschool.com>
2021-12-01 23:44:44 -0500

    jeged

--- 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -1,25 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-namespace _100_jagged_array
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int [][] jaggedArray = new int[][]
         {
-            int[][] jaggedArray = new int[][]
-            {
-                new int[] {1, 2, 3},
-                new int[] {0, 1, 2, 3, 4, 5, 6},
-                new int[] {0, 1}
-            };
-            for (int i = 0; i < jaggedArray.Length; i++)
-            {
-                for (int j = 0; j < jaggedArray[i].Length; j++)
-                    if (j < jaggedArray[i].Length -1)
-                        Console.Write("{0} ", jaggedArray[i][j]);
-                    else
-                        Console.Write("{0}\n", jaggedArray[i][j]);
-            }
+            new int[] {0, 1, 2, 3},
+            new int[] {0, 1, 2, 3, 4, 5, 6},
+            new int[] {0, 1}
+        };
+        for (int i = 0; i < jaggedArray.Length; i++)
+        {
+            for (int j = 0; j < jaggedArray[i].Length; j++)
+                if (j < jaggedArray[i].Length - 1)
+                    Console.Write("{0} ", jaggedArray[i][j]);
+                else
+                    Console.Write("{0}\n", jaggedArray[i][j]);
         }
     }
 }
