7b8b153837b6555ec5b0f03ea46647e18a3edad5
dantereto <2247@holbertonschool.com>
2021-12-02 19:26:01 -0500

    kinked node

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int index = 0;
+        foreach(int data in myLList){
+            if (index == n)
+                return data;
+            index++;
+        }
+        return 0;
+    }
+}
