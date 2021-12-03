1078838fe9dfe14c7998adb09da775478ab8150a
dantereto <2247@holbertonschool.com>
2021-12-02 22:59:35 -0500

    llist delete

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        LinkedListNode<int> del = myLList.First;
+        for (int i = 0; i < index && del.Next != null; i++)
+            del = del.Next;
+        if (index == index)
+            myLList.Remove(del); 
+    }
+}
