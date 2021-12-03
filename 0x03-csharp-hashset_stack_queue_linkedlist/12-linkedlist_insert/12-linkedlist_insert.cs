42494517747c7c3b3156b1c07dd2428bd273f732
dantereto <2247@holbertonschool.com>
2021-12-02 20:33:25 -0500

    inserr

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> new_n = new LinkedListNode<int>(n);
+        LinkedListNode<int> node = myLList.First;
+        while (node != null)
+        {
+            if (node.Value > n)
+            {
+                if (node.Next == null)
+                {
+                    myLList.AddAfter(node, new_n);
+                    break;
+                }
+                else 
+                {
+                    myLList.AddBefore(node, new_n);
+                    break;
+                }
+            }
+            node = node.Next;
+        }
+        return (new_n);
+    }
+}
