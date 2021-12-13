df6ac83f04469f87df3a4a2fa7f8bcf0a544857a
dantereto <2247@holbertonschool.com>
2021-12-13 14:36:06 -0500

    divide list

--- 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -5,15 +5,16 @@ class List
     public static int SafePrint(List<int> myList, int n)
     {
         int i = 0;
-        try
+        for(i = 0; i < n; i++)
         {
-            for(; i < n; i++)
+            try
             {
                 Console.WriteLine(myList[i]);
             }
-        }
-        catch (Exception){
-
+            catch (Exception)
+            {
+                return i;
+            }
         }
         return i;
     }
