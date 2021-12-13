3d5cc9cce053016c796efbcfef9064519e5a7287
dantereto <2247@holbertonschool.com>
2021-12-13 18:10:43 -0500

    dog

---
+++ 0x05-csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,19 @@
+﻿public enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+public struct Dog{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+    public Dog(string nam, float ag, string owne, Rating rat)
+    {
+        name = nam;
+        age = ag;
+        owner = owne;
+        rating = rat;
+    }
+}
