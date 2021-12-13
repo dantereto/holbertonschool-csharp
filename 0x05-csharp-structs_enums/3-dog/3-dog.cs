7e691b0eb2dcb1ac44ae6611c22b988726e0936e
dantereto <2247@holbertonschool.com>
2021-12-13 18:19:18 -0500

    dog

---
+++ 0x05-csharp-structs_enums/3-dog/3-dog.cs
@@ -0,0 +1,23 @@
+﻿public enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+public struct Dog{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+    public Dog(string nam, float ag, string owne, Rating rat)
+    {
+        name = nam;
+        age = ag;
+        owner = owne;
+        rating = rat;
+    }
+    public override string ToString()
+   {
+      return string.Format("Dog Name: {0}\nAge: {1}\nOwner: {2}\nRating: {3}", name, age, owner, rating);
+   }
+}
