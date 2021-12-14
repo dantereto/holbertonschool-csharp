e3bf6f63d2485e64dd384fe6395eebf4c48bd37c
dantereto <2247@holbertonschool.com>
2021-12-14 16:43:02 -0500

    enemy

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace Enemies
+{
+    /// <summary>
+    /// Class of enemies
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary> Zombie health</summary>
+        public int health;
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
+        public Zombie(){
+            health = 0;
+        }
+    }
+}
