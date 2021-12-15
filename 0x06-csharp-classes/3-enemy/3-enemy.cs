5e3ca8d3ba8b6082bd3a598b0e1ef69ad4484644
dantereto <2247@holbertonschool.com>
2021-12-14 20:08:45 -0500

    enemy

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace Enemies
+{
+    /// <summary>
+    /// Class of enemies
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary> Zombie health</summary>
+        private int health;
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
+        public Zombie(){
+            health = 0;
+        }
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            else
+                this.health = value;
+        }
+        public int GetHealth(){
+            return this.health;
+        }
+    }
+}
