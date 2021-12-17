d500b0a8562b0dc7cc8162a5dfaaf3f811dad32b
dantereto <2247@holbertonschool.com>
2021-12-16 22:23:54 -0500

    camelcase

---
+++ 0x07-csharp-tdd/5-camelcase/Text.Tests/Text.Tests.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+
+namespace Text.Tests
+{
+    public class Tests
+    {
+
+        [Test]
+        public void Test1()
+        {
+            string c = "hiHiHiHi";
+            Assert.AreEqual(Str.CamelCase(c), 4);
+        }
+        [Test]
+        public void TestCamel()
+        {
+            string c = "hihello";
+            Assert.AreEqual(Str.CamelCase(c), 1);
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/5-camelcase/Text/Text.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Text
+{
+    /// <summary> Class of Text</summary>
+    public class Str
+    {
+        /// <summary> Count the camelcase</summary>
+        public static int CamelCase(string s)
+        {
+            int count = 1;
+            for (int i = 1; i < s.Length - 1; i++)
+            {
+                if (s[i] >= 65 && s[i] <= 90)
+                        count++;
+            }
+            return count;
+        }
+    }
+}
