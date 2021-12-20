2fbb98acb68e43cdb2005359d3cf70193e5a43c8
dantereto <2247@holbertonschool.com>
2021-12-20 17:59:13 -0500

    subclass

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+/// <summary> Class of obj</summary>
+class Obj
+{
+    /// <summary> check sub class</summary>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        if (derivedType.IsSubclassOf(baseType))
+        {
+            return true;
+        }
+        return false;
+    }
+}
