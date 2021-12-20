5ca8118223ce9ca88e10db9c5520ccd661549bf8
dantereto <2247@holbertonschool.com>
2021-12-20 17:07:21 -0500

    check array

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+/// <summary> Class of obj</summary>
+class Obj
+{
+    /// <summary> Bool for array</summary>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        if (obj.GetType() == typeof(Array) || obj is Array)
+        {
+            return true;
+        }
+        return false;
+    }
+}
