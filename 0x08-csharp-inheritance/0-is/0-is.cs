735284e0f7f500fb4e92bb1258a620d185b0ea4d
dantereto <2247@holbertonschool.com>
2021-12-20 16:28:33 -0500

    check int

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+/// <summary> Class of obj</summary>
+class Obj
+{
+    /// <summary> Class of operations</summary>
+    public static bool IsOfTypeInt(object obj){
+        if (obj.GetType() == typeof(int))
+        {
+            return true;
+        }
+        return false;
+    }
+}
