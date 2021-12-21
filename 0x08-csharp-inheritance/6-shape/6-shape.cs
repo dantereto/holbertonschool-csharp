fc512a68ead269b882b72c7c4099100af9eb7854
dantereto <2247@holbertonschool.com>
2021-12-20 19:58:51 -0500

    rectangle shape

---
+++ 0x08-csharp-inheritance/6-shape/6-shape.cs
@@ -0,0 +1,34 @@
+﻿using System;
+/// <summary> shape class</summary>
+class Shape
+{
+    /// <summary> throw exepction</summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+/// <summary> Rectangle class</summary>
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+    public int Width{
+    get { return width; }
+    set {
+        if (value < 0)
+            throw new ArgumentException("Width must be greater than or equal to 0");
+        else
+            width = value; 
+    }
+    }
+    public int Height{
+        get { return height; }
+        set {
+            if (value < 0)
+            throw new ArgumentException("Height must be greater than or equal to 0");
+        else
+            height = value; 
+        }
+    }
+}
