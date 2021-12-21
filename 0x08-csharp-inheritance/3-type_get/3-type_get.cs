c9e2599be86d7509eab58988e849b629227f31ad
dantereto <2247@holbertonschool.com>
2021-12-20 19:17:40 -0500

    print info type

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+/// <summary> Class of obj</summary>
+
+class Obj
+{
+    /// <summary> Print info</summary>
+
+    public static void Print(object myObj)
+    {
+        TypeInfo t = myObj.GetType().GetTypeInfo();
+        Console.WriteLine("{0} Properties:", t.Name);
+        foreach (PropertyInfo p in t.GetProperties())
+        {
+            Console.WriteLine(p.Name);
+        }
+        Console.WriteLine("{0} Methods:", t.Name);
+        foreach (MethodInfo m in t.GetMethods())
+        {
+            Console.WriteLine(m.Name);
+        }
+        
+    }
+}
