646ad3c428b906e655d0365ead2ebe78e8fca955
dantereto <2247@holbertonschool.com>
2021-12-20 19:40:25 -0500

    shape

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,10 @@
+﻿using System;
+/// <summary> shape class</summary>
+class Shape
+{
+    /// <summary> throw exepction</summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
