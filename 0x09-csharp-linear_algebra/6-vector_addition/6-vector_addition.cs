0a3cd2b547d58ad1e20bfbc912c2550c7f644733
dantereto <2247@holbertonschool.com>
2022-01-17 16:00:58 -0500

    aditions

--- 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -5,7 +5,7 @@ class VectorMath
     /// <summary> double to sum vectors</summary>
     public static double[] Add(double[] vector1, double[] vector2)
     {
-        if (vector1.Length == 2 || vector1.Length == 3 || vector1.Length == vector2.Length)
+        if ((vector1.Length == 2 || vector1.Length == 3) && vector1.Length == vector2.Length)
         {
             for (var i = 0; i < vector1.Length; i++)
                 vector1[i] += vector2[i];
