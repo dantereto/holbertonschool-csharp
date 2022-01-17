c9eb8652d4a3953b05db6bd43877eccb7c6f3fbb
dantereto <2247@holbertonschool.com>
2022-01-17 13:37:21 -0500

    magnitude function

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -7,10 +7,10 @@ class VectorMath
     {
         if (vector.Length == 2 || vector.Length == 3)
         {
-        double sum = 0;
-        foreach(var value in vector)
-            sum += Math.Pow(value, 2);
-        return Math.Round(Math.Sqrt(sum), 2);
+            double sum = 0;
+            foreach(var value in vector)
+                sum += Math.Pow(value, 2);
+            return Math.Round(Math.Sqrt(sum), 2);
         }
         return -1;
     }
