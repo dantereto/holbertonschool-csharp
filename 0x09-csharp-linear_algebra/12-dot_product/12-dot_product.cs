332b44d1b92eb6de1c8d606e93b7d187fae9db11
dantereto <2247@holbertonschool.com>
2022-01-17 18:37:27 -0500

    dot product

---
+++ 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class VectorMath
+{
+    public static double DotProduct(double[] vector1, double[] vector2)
+    {
+        double result = 0;
+        if ((vector1.Length == 2 || vector1.Length == 3) && vector1.Length == vector2.Length)
+        {
+            for (var i = 0; i < vector1.Length; i++)
+            {
+                vector1[i] *= vector2[i];
+                result += vector1[i];
+            }
+            return result;
+        }
+        return -1;
+    }
+}
---
+++ 0x09-csharp-linear_algebra/12-dot_product/12-main.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+static void Main(string[] args)
+    {
+        double[] vector1 = {9, 5};
+        double[] vector2 = {1, 2};
+        Console.WriteLine(VectorMath.DotProduct(vector1, vector2));
+    }
+}
