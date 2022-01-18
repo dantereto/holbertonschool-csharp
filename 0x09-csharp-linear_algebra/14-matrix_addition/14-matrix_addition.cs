841ac4099c8a7b52e997e8294a07530d4a1a9b5b
dantereto <2247@holbertonschool.com>
2022-01-17 21:46:45 -0500

    addition

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -7,12 +7,14 @@ class MatrixMath
     {
         if (matrix1.GetLength(0) == 2 && matrix1.GetLength(1) == 2 ||
             matrix1.GetLength(0) == 3  &&matrix2.GetLength(1) == 3||
-            matrix1.Length == matrix2.Length)
+            matrix1.GetLength(0) == matrix2.GetLength(0))
         {
             for (int i = 0; i < matrix1.GetLength(0); i++)
             {
                 for (int j = 0; j < matrix2.GetLength(1); j++)
+                {
                     matrix1[i, j] += matrix2[i, j];
+                }
             }
             return matrix1;
         }
