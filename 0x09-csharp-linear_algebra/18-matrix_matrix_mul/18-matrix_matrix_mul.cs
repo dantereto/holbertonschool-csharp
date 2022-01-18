b9476a53f09c5361f9a467ede2f2e3f85d39e3d4
dantereto <2247@holbertonschool.com>
2022-01-18 13:02:27 -0500

    matrix

--- 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -6,9 +6,9 @@ class MatrixMath
     public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
     {
         double [,] result; 
-        if (((matrix1.GetLength(0) == 2 && matrix1.GetLength(1) == 2) ||
-            (matrix1.GetLength(0) == 3  &&matrix2.GetLength(1) == 3) &&
-            matrix1.Length == matrix2.Length))
+        if (matrix1.Length != 0 ||
+            matrix2.Length != 0 ||
+            matrix1.GetLength(1) == matrix2.GetLength(0))
         {
             result = new double[matrix1.GetLength(0), matrix2.GetLength(1)];
             for (int i = 0; i < matrix1.GetLength(0); i++)
