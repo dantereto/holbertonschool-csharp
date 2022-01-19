083882f2b965db486d3a58310d447075455f6eef
dantereto <2247@holbertonschool.com>
2022-01-19 18:43:32 -0500

    cross product

--- 0x09-csharp-linear_algebra/30-cross_product/30-cross_product.cs
+++ 0x09-csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -7,11 +7,11 @@ class VectorMath
     {
         if (vector1.Length == 3 || vector2.Length == 3)
         {
-            return new double[3] {
-                vector1[1] * vector2[2] - vector1[2] * vector2[1],
-                vector1[2] * vector2[0] - vector1[0] * vector2[2],
-                vector1[0] * vector2[1] - vector1[1] * vector2[0]
-            };
+            double x = vector1[1] * vector2[2] - vector1[2] * vector2[1];
+            double y = vector1[2] * vector2[0] - vector1[0] * vector2[2];
+            double z = vector1[0] * vector2[1] - vector1[1] * vector2[0];
+            var res = new double[]{x, y, z};
+            return res;
         }
         return new double[1] { -1 };
     }
