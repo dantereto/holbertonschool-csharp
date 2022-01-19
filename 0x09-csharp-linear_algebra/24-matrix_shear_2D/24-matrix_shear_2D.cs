5fb26fbef54f7c7682b8c85807658a17a4d03d1e
dantereto <2247@holbertonschool.com>
2022-01-18 19:46:01 -0500

    shear

---
+++ 0x09-csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -0,0 +1,32 @@
+﻿using System;
+/// <summary> Class of matrix</summary>
+class MatrixMath
+{
+    /// <summary>double for matrix scalar</summary>
+    public static double[,] Shear2D(double[,] matrix, char direction, double factor)
+    {
+        if (matrix.GetLength(0) == 2 || matrix.GetLength(1) == 2)
+        {
+            double [,] shear = new double[,] {{1, 0}, {0, 1}};
+            if (direction == 'x')
+                shear[1, 0] = factor;
+            else if (direction == 'y')
+                shear[0, 1] = factor;
+            else
+                return new double[,] {{-1}};
+            double [,] result = new double[2, 2];
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    double sum = 0;
+                    for (int k = 0; k < 2; k++)
+                        sum += matrix[i, k] * shear[k, j];
+                    result[i, j] = Math.Round(sum, 2);
+                }
+            }
+            return result;
+        }
+        return new double[,] {{-1}};
+    }
+}
