1c675967a91ead9f3ae3d6f028ad2172ed2593d7
dantereto <2247@holbertonschool.com>
2022-01-20 22:37:25 -0500

    determinat

---
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double Determinant(double[,] matrix)
+    {
+        if (matrix.GetLength(0) == 2)
+        {
+            return Math.Round(matrix[0, 0] * matrix[1, 1] - matrix[1, 0], 2);
+        }
+
+        else if (matrix.GetLength(0) == matrix.GetLength(1) ||
+            matrix.GetLength(0) < 2 || matrix.GetLength(0) < 3)
+        {
+            double sum = 0;
+            sum += matrix[0, 0] * (matrix[1, 1] * matrix[2, 2] - matrix[2, 1] * matrix[1, 2]);
+            sum += matrix[0, 1] * (matrix[1, 0] * matrix[2, 2] - matrix[2, 0] * matrix[1, 2]);
+            sum += matrix[0, 2] * (matrix[1, 0] * matrix[2, 1] - matrix[2, 0] * matrix[1, 1]);
+            return Math.Round(sum, 2);
+        }
+        return -1;
+    }
+}
