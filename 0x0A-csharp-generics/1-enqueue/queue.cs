18585800bc3c3b1eee8193f2300b1d21c8626e33
dantereto <2247@holbertonschool.com>
2022-03-21 18:24:48 -0500

    enqueue

---
+++ 0x0A-csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Queue<T>
+{
+    Node head = null;
+    Node tail = null;
+    int count = 0;
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+    public class Node
+    {
+        public object value = null;
+        public Node next = null;
+        public object Value{
+            get { return value; }
+            set { this.value = value; }
+        }
+    }
+    public void Enqueue(T value)
+    {
+        Node node = new Node();
+        node.Value = value;
+
+        if (head == null)
+        {
+            head = node;
+            tail = node;
+        }
+        else
+        {
+            tail.next = node;
+            tail = node;
+        }
+        count++;
+    }
+    public int Count()
+    {
+        return (count);
+    }
+}
