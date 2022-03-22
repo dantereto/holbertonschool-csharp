fc2d3b1a75a538ce2edd9ba6d68f4207fa8c3fa1
dantereto <2247@holbertonschool.com>
2022-03-21 20:07:34 -0500

    print

---
+++ 0x0A-csharp-generics/4-print/queue.cs
@@ -0,0 +1,78 @@
+﻿using System;
+class Queue<T>
+{
+    Node head = null;
+    Node tail = null;
+    int count = 0;
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+    public class Node
+    {
+        public object value = null;
+        public Node next = null;
+        public object Value{
+            get { return value; }
+            set { this.value = value; }
+        }
+    }
+    public void Enqueue(T value)
+    {
+        Node node = new Node();
+        node.Value = value;
+
+        if (head == null)
+        {
+            head = node;
+            tail = node;
+        }
+        else
+        {
+            tail.next = node;
+            tail = node;
+        }
+        count++;
+    }
+    public T Dequeue()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+        Node temp = head;
+        head = head.next;
+        count--;
+        return (T)temp.value;
+    }
+    public T Peek()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+        Node temp = head;
+        return (T)temp.value;
+    }
+    public T Print()
+    {
+        Node load = head;
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+        while (load != null)
+        {
+            Console.WriteLine(load.Value);
+            load = load.next;
+        };
+        return default(T);
+    }
+    public int Count()
+    {
+        return (count);
+    }
+}
