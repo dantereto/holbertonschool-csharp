4f036cb22506c0f7aaf4b9510112b05b805b4dde
dantereto <2247@holbertonschool.com>
2022-03-22 14:55:19 -0500

    abstract class

--- 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -3,10 +3,9 @@
 abstract class Base
 {
     public string name {get; set;}
-    
+
     public override string ToString()
     {
         return (String.Format("{0} is a {1}", name, this.GetType().ToString()));
     }
 }
-
