e9ad5b96dfd6283512d2561d54743959675b5c59
dantereto <2247@holbertonschool.com>
2022-03-22 17:25:54 -0500

    interface

---
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+abstract class Base
+{
+    public string name {get; set;}
+    
+    public override string ToString()
+    {
+        return (String.Format("{0} is a {1}", name, this.GetType().ToString()));
+    }
+}
+    interface IInteractive
+    {
+        void Interact();
+    }
+    interface IBreakable{
+        int durability {get; set;}
+        void Break();
+    }
+    interface ICollectable{
+        bool isCollected {get; set;}
+        void Collect();
+    }
+    class TestObject : Base, IInteractive, IBreakable, ICollectable {
+    
+    public int durability {get; set; }
+    public bool isCollected {get; set; }
+    public void Interact() {}
+    public void Break() {}
+    public void Collect() {}
+}
