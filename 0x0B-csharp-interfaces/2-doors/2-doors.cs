808a2b181a45c29a1377333b0e9b0fc8fbe06bf7
dantereto <2247@holbertonschool.com>
2022-03-22 19:32:16 -0500

    doors

---
+++ 0x0B-csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+abstract class Base
+{
+    public string name {get; set;}
+    
+    public override string ToString()
+    {
+        return (String.Format("{0} is a {1}", name, this.GetType().ToString()));
+    }
+}
+    interface IInteractive
+    {
+        void Interact();
+    }
+    interface IBreakable{
+        int durability {get; set;}
+        void Break();
+    }
+    interface ICollectable{
+        bool isCollected {get; set;}
+        void Collect();
+    }
+    
+    class Door : Base, IInteractive{
+        public Door(string name = "Door")
+        {
+            this.name = name;
+        }
+        public void Interact(){
+            Console.WriteLine("You try to open the {0}. It's locked.", this.name);
+        }
+    }
