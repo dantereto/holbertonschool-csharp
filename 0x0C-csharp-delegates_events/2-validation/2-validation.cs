ff143bc3cd8c77fea17321288ddbb582075d7153
dantereto <2247@holbertonschool.com>
2022-03-24 14:08:30 -0500

    validation

--- 0x0C-csharp-delegates_events/2-validation/2-validation.cs
+++ 0x0C-csharp-delegates_events/2-validation/2-validation.cs
@@ -28,7 +28,7 @@ public class Player
     /// <summary> Player Class </summary>
     public void TakeDamage(float damage)
     {
-        if (damage < 0)
+        if (damage <= 0)
         {
             Console.WriteLine($"{name} takes 0 damage!");
             return;
@@ -39,13 +39,13 @@ public class Player
     /// <summary> Player Class </summary>
     public void HealDamage(float heal)
     {
-        if (heal < 0)
+        if (heal <= 0)
         {
             Console.WriteLine($"{name} heals 0 HP!");
             return;
         }
-        ValidateHP(this.hp + heal);
         Console.WriteLine($"{name} heals {heal} HP!");
+        ValidateHP(this.hp + heal);
     }
     /// <summary> Player Class </summary>
     public void ValidateHP(float newHp)
