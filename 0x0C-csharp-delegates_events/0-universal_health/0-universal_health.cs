24e98b6ff75e8ee09897dbc8b61a14c8c4ba2938
dantereto <2247@holbertonschool.com>
2022-03-23 19:07:50 -0500

    player

---
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,25 @@
+﻿using System;
+/// <summary> Player Class </summary>
+public class Player
+{
+    private string name {get; set;}
+    private float maxHp {get; set;}
+    private float hp {get; set;}
+    /// <summary> Player Class </summary>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        this.name = name;
+        if (maxHp <= 0)
+        {
+            maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        this.maxHp = maxHp;
+        this.hp = maxHp;
+    }
+    /// <summary> Player Class </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine("{0} has {1} / {2} health", name, hp, maxHp);
+    }
+}
