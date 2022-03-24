b2b92105bcc249eab4ec6bfc55575c4743fbbee0
dantereto <2247@holbertonschool.com>
2022-03-23 20:41:40 -0500

    damage

---
+++ 0x0C-csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+/// <summary> Player Class </summary>
+public delegate float CalculateHealth(float amount);
+/// <summary> Player Class </summary>
+public class Player
+{
+    
+    private string name {get; set;}
+    private float maxHp {get; set;}
+    private float hp {get; set;}
+    /// <summary> Player Class </summary>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        this.name = name;
+        if (maxHp <= 0)
+        {
+            maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        this.maxHp = maxHp;
+        this.hp = maxHp;
+    }
+    /// <summary> Player Class </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine("{0} has {1} / {2} health", name, hp, maxHp);
+    }
+    /// <summary> Player Class </summary>
+    public void TakeDamage(float damage)
+    {
+        if (damage <= 0)
+        {
+            Console.WriteLine($"{name} takes 0 damage!");
+            return;
+        }
+        Console.WriteLine($"{name} takes {damage} damage!");
+    }
+    /// <summary> Player Class </summary>
+    public void HealDamage(float heal)
+    {
+        if (heal <= 0)
+        {
+            Console.WriteLine($"{name} heals 0 HP!");
+            return;
+        }
+        Console.WriteLine($"{name} heals {heal} HP!");
+    }
+}
